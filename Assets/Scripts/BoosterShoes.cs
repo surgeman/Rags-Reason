768fdf647798969a9a8a13feba8da215d677ae3d
Sergio Betancourt <zuelablues@gmail.com>
2015-11-05 17:25:38 -0800

    added adjustable boost counter

--- Assets/Scripts/BoosterShoes.cs
+++ Assets/Scripts/BoosterShoes.cs
@@ -4,26 +4,31 @@ using UnityStandardAssets.Characters.ThirdPerson;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class BoosterShoes : UtilityTools {
-
+	public int boostCounter = 3;
+	private int boostLimit = 0;
 	ThirdPersonCharacter Rags;
 
 	// Use this for initialization
 	void Start () {
 		Rags = GameManager.god.ragsObject;
-
+		boostLimit = boostCounter;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Rags.m_IsGrounded == false)
-		{
-			if (Input.GetButtonDown("Jump"))
-			{
-				Rags.jumpNow();
+		//Checks if Rags is in the air
+		if (Rags.m_IsGrounded == false) {
+			//Sets the amount of boost jumps
+			if (boostCounter > 0) {
+				if (Input.GetButtonDown ("Jump")) {
+					Rags.jumpNow ();
+					boostCounter--;
+				}
 			}
+		//Resets the boost counter once you land	
+		} else if (Rags.m_IsGrounded == true) {
+			boostCounter = boostLimit;
 		}
-
-			}
-
 	}
+}
